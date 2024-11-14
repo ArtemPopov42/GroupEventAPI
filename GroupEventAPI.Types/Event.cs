2aec2f327b1bccf0e8aec1ea9710d576db69f24d
Talkontar <artempopovartempopov@gmail.com>
2024-11-14 13:45:56 +0200

    add user, event types

---
+++ GroupEventAPI.Types/Event.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupEventAPI.Types
+{
+    internal class Event
+    {
+        public string Description { get; set; }
+        User _creator;
+        DateTime _date;
+
+
+        public Event(User user, string description = "", DateTime date = default)
+        {
+            Description = description;
+            _creator = user;
+            _date = date;
+        }
+    }
+}
---
+++ GroupEventAPI.Types/User.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupEventAPI.Types
+{
+    internal class User: IdentityUser
+    {
+        List<Event> _events;
+
+        public User(string email, string username):base()
+        {
+            //identityUser fields
+            Email = email;
+            UserName = username;
+            //
+
+            _events = new List<Event>();
+        }
+    }
+}
